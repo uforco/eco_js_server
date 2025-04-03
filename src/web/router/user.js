c9876fe1e766be7d67701238df4b155892b22ad0
MD Sharif <srka780@gmail.com>
2025-04-03 11:36:54 +0600

    off user url

--- src/web/router/user.js
+++ src/web/router/user.js
@@ -5,15 +5,15 @@ import UserSystem from "../controllers/UserSystem.js";
 // Open url with users 
 
 // user upser api system
-userDetails.route("/oauth_check").post(UserSystem.oauthChecking);
-userDetails.route("/singup").post(UserSystem.singupUser);
-userDetails.route("/signin").post(UserSystem.loginprovider);
+// userDetails.route("/oauth_check").post(UserSystem.oauthChecking);
+// userDetails.route("/singup").post(UserSystem.singupUser);
+// userDetails.route("/signin").post(UserSystem.loginprovider);
 
 
 // forget passward
-userDetails.route("/forget_passwoard/create-link").post(UserSystem.forgetPasswoard);
-userDetails.route("/forget_passwoard/check-url/:token").get(UserSystem.forgetPasswoard_check_token);
-userDetails.route("/forget_passwoard/submit_passward/:token").post(UserSystem.forget_submit_password);
+// userDetails.route("/forget_passwoard/create-link").post(UserSystem.forgetPasswoard);
+// userDetails.route("/forget_passwoard/check-url/:token").get(UserSystem.forgetPasswoard_check_token);
+// userDetails.route("/forget_passwoard/submit_passward/:token").post(UserSystem.forget_submit_password);
 
 
 
