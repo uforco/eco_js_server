7c9ec663ebd264e9f0986b0a2c3926e148adf0bc
MD Sharif <srka780@gmail.com>
2025-04-03 11:51:05 +0600

    function off user system

--- src/web/controllers/userSystem.js
+++ src/web/controllers/userSystem.js
@@ -1,9 +1,9 @@
 
-import prisma from "../../DB/db.config.js";
-// const { Prisma } = pkg
-import registerUserService from "../services/user/registerUser.service.js";
-import loginUserService from "../services/user/loginUser.Service.js";
-import OauthDataChecking from '../services/user/oauthChecking.service.js';
+// import prisma from "../../DB/db.config.js";
+
+// import registerUserService from "../services/user/registerUser.service.js";
+// import loginUserService from "../services/user/loginUser.Service.js";
+// import OauthDataChecking from '../services/user/oauthChecking.service.js';
 import forgetPasswoardSendEmailVarify from "../services/user/forgetPasswoardSendEmailVarify.js";
 import forgetPasswoardCheckToken from "../services/user/forgetPasswoardCheckToken.js";
 import forgetSubmitNewPassword from "../services/user/forgetSubmitNewPassword.js";
@@ -11,54 +11,54 @@ import forgetSubmitNewPassword from "../services/user/forgetSubmitNewPassword.js
 class UserSystem {
 
 
-  static async oauthChecking(req, res) {
-      try{
-        const useridentity = await OauthDataChecking(req.body);
-        if (useridentity && useridentity.success) {
-          return res.status(200).send(useridentity);
-        }
-        else{
-          return res.status(406).send(useridentity);
-        }
-      }catch(err){
-        console.log('oauthChecking Error ===', err )
-        return res.status(500).send("something is wrong");
-      } 
-  }
+  // static async oauthChecking(req, res) {
+  //     try{
+  //       const useridentity = await OauthDataChecking(req.body);
+  //       if (useridentity && useridentity.success) {
+  //         return res.status(200).send(useridentity);
+  //       }
+  //       else{
+  //         return res.status(406).send(useridentity);
+  //       }
+  //     }catch(err){
+  //       console.log('oauthChecking Error ===', err )
+  //       return res.status(500).send("something is wrong");
+  //     } 
+  // }
 
-  // TODO: get signup data with font end
-  static async singupUser(req, res) {
-    try{
-      const newUserData = req.body;
-      if(!newUserData.email || !newUserData.password || !newUserData.firstName ) {
-        return res.status(400).send({ success: false, message: " 🤬🤬 fill all is required 🤬🤬 " });
-      }
-      // Check if the user already exists in the database
-      const isExistsUser = await prisma.user.findFirst({where: {email: newUserData.email},});
-      if (isExistsUser) {
-        return res.status(409).send({ success: false, message: "You have Already account"});
-      }
-      // DB service
-      const newUser = await registerUserService(newUserData);
-      if(newUser.success){
-        return res.status(200).send(newUser)
-      }
-    }catch(err){
-      console.error("Error creating user - singupUser controller :", err);
-      res.status(500).send({ success: false, error: "Internal Server Error signup" });
-    }
-  }
+  // // TODO: get signup data with font end
+  // static async singupUser(req, res) {
+  //   try{
+  //     const newUserData = req.body;
+  //     if(!newUserData.email || !newUserData.password || !newUserData.firstName ) {
+  //       return res.status(400).send({ success: false, message: " 🤬🤬 fill all is required 🤬🤬 " });
+  //     }
+  //     // Check if the user already exists in the database
+  //     const isExistsUser = await prisma.user.findFirst({where: {email: newUserData.email},});
+  //     if (isExistsUser) {
+  //       return res.status(409).send({ success: false, message: "You have Already account"});
+  //     }
+  //     // DB service
+  //     const newUser = await registerUserService(newUserData);
+  //     if(newUser.success){
+  //       return res.status(200).send(newUser)
+  //     }
+  //   }catch(err){
+  //     console.error("Error creating user - singupUser controller :", err);
+  //     res.status(500).send({ success: false, error: "Internal Server Error signup" });
+  //   }
+  // }
 
-  static async loginprovider (req, res) {
-    //TODO: form data validation
-    const { provider, email, password } =  req.body
-    try{
-      return await loginUserService(req, res)
-    }catch(err){
-      console.log(' loginprovider :', err)
-      res.status(500).send({success: false, error: 'something worg'})
-    }
-  }
+  // static async loginprovider (req, res) {
+  //   //TODO: form data validation
+  //   const { provider, email, password } =  req.body
+  //   try{
+  //     return await loginUserService(req, res)
+  //   }catch(err){
+  //     console.log(' loginprovider :', err)
+  //     res.status(500).send({success: false, error: 'something worg'})
+  //   }
+  // }
 
 
 
