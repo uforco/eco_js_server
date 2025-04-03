4b53f22489b65e6230685b714cbeafb515032eef
MD Sharif <srka780@gmail.com>
2025-04-03 11:55:58 +0600

    function off user system 444444

--- src/web/router/user.js
+++ src/web/router/user.js
@@ -1,19 +1,19 @@
 import userDetails from "../../lib/Routing.js";
-import UserSystem from './../controllers/userSystem';
+import UserSystem from './../controllers/userSystem.js';
 
 
 // Open url with users 
 
 // user upser api system
 userDetails.route("/oauth_check").post(UserSystem.oauthChecking);
-// userDetails.route("/singup").post(UserSystem.singupUser);
-// userDetails.route("/signin").post(UserSystem.loginprovider);
+userDetails.route("/singup").post(UserSystem.singupUser);
+userDetails.route("/signin").post(UserSystem.loginprovider);
 
 
 // forget passward
-// userDetails.route("/forget_passwoard/create-link").post(UserSystem.forgetPasswoard);
-// userDetails.route("/forget_passwoard/check-url/:token").get(UserSystem.forgetPasswoard_check_token);
-// userDetails.route("/forget_passwoard/submit_passward/:token").post(UserSystem.forget_submit_password);
+userDetails.route("/forget_passwoard/create-link").post(UserSystem.forgetPasswoard);
+userDetails.route("/forget_passwoard/check-url/:token").get(UserSystem.forgetPasswoard_check_token);
+userDetails.route("/forget_passwoard/submit_passward/:token").post(UserSystem.forget_submit_password);
 
 
 
